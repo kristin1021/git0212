2531885f3c96faa7707118523dfe88d57dbd9422
xpp <2923596926@qq.com>
2022-06-15 21:12:08 +0800

    push test commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,6 +8,8 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("master test git3");
         System.out.println("hot fix test");
+        System.out.println("push test");
     }
 }
+
 
