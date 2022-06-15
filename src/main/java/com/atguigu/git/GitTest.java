d69ec31f5f8fc8a1a400b2cd78abf06e2a24d541
kristin1021 <103504779+kristin1021@users.noreply.github.com>
2022-06-15 21:26:02 +0800

    Update GitTest.java
    pull test commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("master test git3");
         System.out.println("hot fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
 
